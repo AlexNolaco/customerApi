50986a5b36d424b8464df20ccb99b7c9063df02f
Alex Nolaço <38504933+AlexNolaco@users.noreply.github.com>
2023-04-20 16:01:47 -0300

    add some logs

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -6,19 +6,23 @@ import { Request, Response, NextFunction } from 'express';
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) { }
 
-  @Post() //preciso de auth
+  @Post()
   createCustomer(@Req() req: Request, @Res() res: Response): any {
-    Logger.log("[POST] Create Customer Endpoint Touched");
+    Logger.log('[POST] createCustomer endpoint');
     return this.customersService.createCustomer(req.body)
-      .then((customer) => res.status(HttpStatus.CREATED).send(customer))
+      .then((customer) => {
+        Logger.log('Customer created!')
+        res.status(HttpStatus.CREATED).send(customer)
+      })
       .catch((err) => {
-        Logger.error(err);
+        Logger.error('Customer not created!')
         res.status(HttpStatus.BAD_GATEWAY).send('cache indisponível')
       });
   }
 
   @Put(':id')
   updateCustomerById(@Param('id') id: string, @Req() req: Request, @Res() res: Response): any {
+    Logger.log('[PUT] updateCustomerById endpoint');
     return this.customersService.updateCustomerById(id, req.body)
       .then((customer) => res.status(HttpStatus.CREATED).send(customer))
       .catch((err) => res.status(HttpStatus.BAD_GATEWAY).send('cache indisponível'));

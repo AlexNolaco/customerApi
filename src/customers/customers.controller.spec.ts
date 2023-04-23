0ad5e61dd491235aac1f5c3d308e2ba239fa67cb
Alex Nolaço <38504933+AlexNolaco@users.noreply.github.com>
2023-04-23 13:22:28 -0300

    removed logs and builded unit tests

--- src/customers/customers.controller.spec.ts
+++ src/customers/customers.controller.spec.ts
@@ -1,22 +1,122 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
+import { CustomersController } from './customers.controller';
+import { CreateCustomerRequestModel } from './models/createCustomerRequest';
+import { Response } from 'express';
+import { UpdateCustomerRequestModel } from './models/updateCustomerRequest';
 
-describe('CustomersController', () => {
-  let CustomersController: CustomersController;
+describe('CustomersController Unit Tests', () => {
+  let customersController: CustomersController;
+  const CustomerServiceProvider = {
+    provide: CustomersService,
+    useClass: CustomersServiceMock,
+  };
+  const requestMock = new CreateCustomerRequestModel();
+  const statusResponseMock = {
+    send: jest.fn((x) => x),
+  }
+  const responseMock = {
+    status: jest.fn((x) => statusResponseMock),
+    send: jest.fn((x) => x)
+  } as unknown as Response;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
-      providers: [CustomersService],
+      providers: [CustomersService, CustomerServiceProvider],
     }).compile();
-
-    CustomersController = app.get<CustomersController>(CustomersController);
+    customersController = module.get<CustomersController>(CustomersController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(CustomersController.getHello()).toBe('Hello World!');
+  it('should be defined', async () => {
+    expect(customersController).toBeDefined();
+  });
+
+  describe('endpoint createCustomer Unit Tests', () => {
+    it('should return', async () => {
+      const expected = {
+        'document': 45973631060,
+        'name': 'Alex Sander Nolaço da Silveira',
+        'id': '165ec741-53c4-4034-adac-962c70324979'
+      };
+      const result = await customersController.createCustomer(requestMock, responseMock);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('endpoint getCustomerById Unit Tests', () => {
+    it('should return', async () => {
+      const expected = {
+        'document': 45973631060,
+        'name': 'Alex Sander Nolaço da Silveira',
+        'id': '165ec741-53c4-4034-adac-962c70324979'
+      };
+      const result = await customersController
+        .getCustomerById('165ec741-53c4-4034-adac-962c70324979', responseMock);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  it('badGatewayMessage', async () => {
+    const expected = {
+      'error': 'Bad Gateway',
+      'message': 'cache indisponível',
+      'statusCode': 502,
+    };
+    const result = await customersController.badGatewayMessage(responseMock);
+    expect(result).toEqual(expected);
+  });
+
+  describe('endpoint updateCustomerById Unit Tests', () => {
+    it('should return conflict id', async () => {
+      const expected = {
+        'error': 'Conflict',
+        'message': 'conflito de ID',
+        'statusCode': 409,
+      };
+      var mock = new UpdateCustomerRequestModel();
+      const result = await customersController
+      .updateCustomerById('165ec741-53c4-4034-adac-962c70324979', mock, responseMock);
+      expect(result).toEqual(expected);
+    });
+
+    it('should return customer', async () => {
+      const expected = {
+        'document': 45973631060,
+        'name': 'Alex Sander Nolaço da Silveira',
+        'id': '165ec741-53c4-4034-adac-962c70324979'
+      };
+      var mock = new UpdateCustomerRequestModel();
+      mock.id = '165ec741-53c4-4034-adac-962c70324979';
+      const result = await customersController
+        .updateCustomerById('165ec741-53c4-4034-adac-962c70324979', mock, responseMock);
+      expect(result).toEqual(expected);
     });
   });
 });
+
+class CustomersServiceMock {
+  async createCustomer(req: any, res: any) {
+    return {
+      'document': 45973631060,
+      'name': 'Alex Sander Nolaço da Silveira',
+      'id': '165ec741-53c4-4034-adac-962c70324979'
+    };
+  }
+
+  async getCustomerById(id: any, res: any) {
+    return {
+      'document': 45973631060,
+      'name': 'Alex Sander Nolaço da Silveira',
+      'id': '165ec741-53c4-4034-adac-962c70324979'
+    };
+  }
+
+  async updateCustomerById(id: any, req: any, res: any) {
+    return {
+      'document': 45973631060,
+      'name': 'Alex Sander Nolaço da Silveira',
+      'id': '165ec741-53c4-4034-adac-962c70324979'
+    };
+  }
+}
--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -11,81 +11,69 @@ export class CustomersController {
   constructor(private readonly customersService: CustomersService) { }
 
   @Post()
-  @ApiResponse({ status: 201, description: 'customer'})
-  @ApiResponse({ status: 401, description: 'não autorizado'})
+  @ApiResponse({ status: 201, description: 'customer' })
+  @ApiResponse({ status: 401, description: 'não autorizado' })
   @ApiResponse({ status: 400, description: 'request inválida' })
   @ApiResponse({ status: 502, description: 'cache indisponível / sso indisponivel' })
   createCustomer(@Body() req: CreateCustomerRequestModel, @Res() res: Response): any {
-    Logger.log('[POST] createCustomer endpoint', 'CustomersController');
-    this.customersService.createCustomer(req)
-    .then((customer) => {
-        Logger.log('Created');
+    return this.customersService.createCustomer(req)
+      .then((customer) => {
         return res.status(HttpStatus.CREATED).send(customer)
       })
       .catch((err) => {
-        Logger.error('Cache problem', 'Redis');
         return this.badGatewayMessage(res);
       });
   }
 
   @Get(':id')
-  @ApiResponse({ status: 200, description: 'customer'})
-  @ApiResponse({ status: 401, description: 'não autorizado'})
+  @ApiResponse({ status: 200, description: 'customer' })
+  @ApiResponse({ status: 401, description: 'não autorizado' })
   @ApiResponse({ status: 400, description: 'request inválida' })
   @ApiResponse({ status: 502, description: 'cache indisponível / sso indisponivel' })
   getCustomerById(@Param('id') id: string, @Res() res: Response): any {
-    Logger.log('[GET] getCustomerById endpoint', 'CustomersController');
-    this.customersService.getCustomerById(id)
+    return this.customersService.getCustomerById(id)
       .then((customer) => {
         if (!customer) {
-          Logger.warn('Not Found');
           return res.status(HttpStatus.NOT_FOUND).send({
             "statusCode": HttpStatus.NOT_FOUND,
             "message": "cliente inexistente",
             "error": "Not Found"
           });
         } else {
-          Logger.log('Found');
           return res.status(HttpStatus.OK).send(customer);
         }
       })
       .catch((err) => {
-        Logger.error('Cache problem', 'Redis');
         return this.badGatewayMessage(res);
       });
   }
 
   @Put(':id')
-  @ApiResponse({ status: 200, description: 'customer'})
-  @ApiResponse({ status: 401, description: 'não autorizado'})
+  @ApiResponse({ status: 200, description: 'customer' })
+  @ApiResponse({ status: 401, description: 'não autorizado' })
   @ApiResponse({ status: 400, description: 'request inválida' })
   @ApiResponse({ status: 502, description: 'cache indisponível / sso indisponivel' })
   updateCustomerById(@Param('id') id: string, @Body() req: UpdateCustomerRequestModel, @Res() res: Response): any {
-    Logger.log('[PUT] updateCustomerById endpoint', 'CustomersController');
     if (id !== req.id) {
-      Logger.warn('Id Conflict');
       return res.status(HttpStatus.CONFLICT).send({
         "statusCode": HttpStatus.CONFLICT,
         "message": "conflito de ID",
         "error": "Conflict"
       });
     }
-    this.customersService.updateCustomerById(id, req)
+    return this.customersService.updateCustomerById(id, req)
       .then((customer) => {
         if (!customer) {
-          Logger.warn('Not Found');
           return res.status(HttpStatus.NOT_FOUND).send({
             "statusCode": HttpStatus.NOT_FOUND,
             "message": "cliente inexistente",
             "error": "Not Found"
           });
         } else {
-          Logger.log('Found');
           return res.status(HttpStatus.OK).send(customer)
         }
       })
       .catch((err) => {
-        Logger.error('Cache problem', 'Redis');
         return this.badGatewayMessage(res);
       });
   }
